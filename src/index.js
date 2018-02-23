2121fc30b0e6841970006ded966e798fcd94fbe4
V.Kharitonovitch <morainrallez@gmail.com>
2018-02-23 22:06:14 +0300

    advanced-zero/feature: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let result = 0;
+  let primeNumbers = {};
+  let countOfKeys = 0;
+  let arrayOfKeys = [];
+  let arrayAfterSort;
+  function CompareForSort(first, second)
+{
+    if (first == second)
+        return 0;
+    if (first < second)
+        return -1;
+    else
+        return 1; 
 }
+
+  for (let i = 2; i <= base; i++){
+    let degreeOfNumber = 0;
+    if (base % i == 0){
+      while(base % i == 0){
+        base /=i;
+        degreeOfNumber++;}
+        primeNumbers[i] = degreeOfNumber;
+        countOfKeys++;
+      }
+    }
+  let largestNumber;
+  let factor = 1;
+  if(countOfKeys > 1){
+    for(var key in primeNumbers){
+      arrayOfKeys.push(key);
+    }
+     arrayAfterSort = arrayOfKeys.sort(CompareForSort).reverse();
+     largestNumber = arrayAfterSort[0];
+  }else{
+    largestNumber = Object.keys(primeNumbers)[0];
+  }
+  largestNumber = parseInt(largestNumber);
+  for(let i = largestNumber;i < number; i *= largestNumber){
+    result += Math.floor(number / i);
+  }
+  return Math.floor(result /= primeNumbers[largestNumber]);
+  };
